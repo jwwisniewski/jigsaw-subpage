80421ec1cd33b785c24f91610210ac549adf4e7f
jwwisniewski <j.w.wisniewski@gmail.com>
2020-02-20 23:51:14 +0100

    add namespace to routes

--- routes/routes.php
+++ routes/routes.php
@@ -2,12 +2,12 @@
 Route::prefix('_admin')->group(function () {
 
     Route::prefix('subpage')->group(function () {
-        Route::get('index', array('as' => 'subpage.index', 'uses' => 'SubpageController@index'));
-        Route::get('create', array('as' => 'subpage.create', 'uses' => 'SubpageController@create'));
-        Route::post('store', array('as' => 'subpage.store', 'uses' => 'SubpageController@store'));
-        Route::get('edit/{subpage}', array('as' => 'subpage.edit', 'uses' => 'SubpageController@edit'));
-        Route::post('update/{subpage}', array('as' => 'subpage.update', 'uses' => 'SubpageController@update'));
-        Route::get('destroy/{subpage}', array('as' => 'subpage.destroy', 'uses' => 'SubpageController@destroy'));
+        Route::get('index', array('as' => 'subpage.index', 'uses' => 'jwwisniewski\Jigsaw\Subpage\Controllers\SubpageController@index'));
+        Route::get('create', array('as' => 'subpage.create', 'uses' => 'jwwisniewski\Jigsaw\Subpage\Controllers\SubpageController@create'));
+        Route::post('store', array('as' => 'subpage.store', 'uses' => 'jwwisniewski\Jigsaw\Subpage\Controllers\SubpageController@store'));
+        Route::get('edit/{subpage}', array('as' => 'subpage.edit', 'uses' => 'jwwisniewski\Jigsaw\Subpage\Controllers\SubpageController@edit'));
+        Route::post('update/{subpage}', array('as' => 'subpage.update', 'uses' => 'jwwisniewski\Jigsaw\Subpage\Controllers\SubpageController@update'));
+        Route::get('destroy/{subpage}', array('as' => 'subpage.destroy', 'uses' => 'jwwisniewski\Jigsaw\Subpage\Controllers\SubpageController@destroy'));
     });
 
 });
